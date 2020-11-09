8b5b43ba3d3a7c8b90f538fe3a7cf505f200a69d
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-11-09 16:14:08 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,10 @@
 function forLoop(array) {
   for (i = 0; i < 25; i++) {
     if(i === 1) {
-      return array.push('I am 1 strange loops.')
+      array.push('I am 1 strange loops.')
     } else {
-      return array.push(`I am ${i} strange loops.`)
+      array.push(`I am ${i} strange loops.`)
     }
   }
+  return array
 }
